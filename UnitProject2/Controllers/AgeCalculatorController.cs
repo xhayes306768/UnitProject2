87299f1e47e18fb78fd212db165d92fbd91cb58b
Xavier Hayes <xavier.hayes@southeasttech.edu>
2023-09-15 19:26:15 -0500

    (This is a new project with the same concept of the first(Wasn;t Working)Created a new Age controller, birthdate class and home page.- Xavier

--- UnitProject2/Controllers/AgeCalculatorController.cs
+++ UnitProject2/Controllers/AgeCalculatorController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using UnitProject2.Models;
 
 namespace UnitProject2.Controllers
 {
     public class AgeCalculatorController : Controller
     {
+       // [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            return View(); 
+        }
+
+
+        [HttpPost]
+        public IActionResult CalculateAge(Birthdate model)
+        {
+            if (ModelState.IsValid)
+            {
+                int age = model.CalculateAge();
+                ViewBag.Age = age;
+                return View("AgeResult");
+            }
+
+            return View("Index", model);
         }
     }
 }
